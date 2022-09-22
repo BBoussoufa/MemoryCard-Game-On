96ced6d9ff7e2787e1799e9bd7cd052c58450148
Badr Boussoufa <110860644+BBoussoufa@users.noreply.github.com>
2022-09-21 23:01:47 -0400

    Add files via upload

--- memoryCardGame/README.md
+++ memoryCardGame/README.md
@@ -2,21 +2,18 @@
 
 ## Description
 
-    The game board consists of sixteen cards arranged randomly in a grid. The deck is made up of eight different pairs of cards, each with different symbols on one side.      Each turn:
-      A player flips one card over to reveal its underlying symbol
-      The player then turns over a second card, trying to find the corresponding card with the same symbol
-      If the cards match, both cards stay flipped over
-      If the cards do not match, both cards are returned to their initial hidden state
-      The game ends once all cards have been correctly matched.
+    The game board consists of sixteen cards arranged randomly in a grid. The deck is made up of eight different pairs of cards, each with different symbols on one side. Each turn:
 
-
+    A player flips one card over to reveal its underlying symbol
+    The player then turns over a second card, trying to find the corresponding card with the same symbol
+    If the cards match, both cards stay flipped over. If the cards do not match, both cards are returned to their initial hidden state
+    The game ends once all cards have been correctly matched.
 
 ## Tools I used
 
 - Html
 - CSS
 - JavaScript
-
 
 ## Future plans
 
--- memoryCardGame/script.js
+++ memoryCardGame/script.js
@@ -1,60 +1,82 @@
-const section = document.querySelector('section')
-const playerLivesCount = document.querySelector('span')
-const playerLives = 4;
+const section = document.querySelector("section");
+const playerLivesCount = document.querySelector("span");
+const playerLives = 7;
 
 //link text
 playerLivesCount.textContent = playerLives;
 
-
 // get the object
 
-  const getTheData = () => [ 
-     {imgSrc: './Images/imgNature1.jpg',  name:'nature1'},
-     {imgSrc: './Images/imgNature2.jpg' , name:'nature2'},
-     {imgSrc: './Images/imgNature3.jpg' , name:'nature3'},
-     {imgSrc: './Images/imgNature4.jpg' , name:'nature4'},
-     {imgSrc: './Images/imgNature5.jpg' , name:'nature5'},
-     {imgSrc: './Images/imgNature6.jpg' , name:'nature6'},
-     {imgSrc: './Images/imgNature7.jpg' , name:'nature7'},
-     {imgSrc: './Images/imgNature8.jpg' , name:'nature8'},
-     {imgSrc: './Images/imgNature2.jpg' , name:'nature2'},
-     {imgSrc: './Images/imgNature3.jpg' , name:'nature3'},
-     {imgSrc: './Images/imgNature4.jpg' , name:'nature4'},
-     {imgSrc: './Images/imgNature5.jpg' , name:'nature5'},
-     {imgSrc: './Images/imgNature6.jpg' , name:'nature6'},
-     {imgSrc: './Images/imgNature7.jpg' , name:'nature7'},
-     {imgSrc: './Images/imgNature8.jpg' , name:'nature8'},
-     {imgSrc: './Images/imgNature1.jpg',  name:'nature1'},
+const getTheData = () => [
+  { imgSrc: "./Images/imgNature1.jpg", name: "nature1" },
+  { imgSrc: "./Images/imgNature2.jpg", name: "nature2" },
+  { imgSrc: "./Images/imgNature3.jpg", name: "nature3" },
+  { imgSrc: "./Images/imgNature4.jpg", name: "nature4" },
+  { imgSrc: "./Images/imgNature5.jpg", name: "nature5" },
+  { imgSrc: "./Images/imgNature6.jpg", name: "nature6" },
+  { imgSrc: "./Images/imgNature7.jpg", name: "nature7" },
+  { imgSrc: "./Images/imgNature8.jpg", name: "nature8" },
+  { imgSrc: "./Images/imgNature2.jpg", name: "nature2" },
+  { imgSrc: "./Images/imgNature3.jpg", name: "nature3" },
+  { imgSrc: "./Images/imgNature4.jpg", name: "nature4" },
+  { imgSrc: "./Images/imgNature5.jpg", name: "nature5" },
+  { imgSrc: "./Images/imgNature6.jpg", name: "nature6" },
+  { imgSrc: "./Images/imgNature7.jpg", name: "nature7" },
+  { imgSrc: "./Images/imgNature8.jpg", name: "nature8" },
+  { imgSrc: "./Images/imgNature1.jpg", name: "nature1" },
 ];
-  const randomize = () => {
-    cardData = getTheData();
-    console.log(cardData);
-    cardData.sort(() => Math.random() - 0.4);
-    return cardData;
-    //console.log(cardData);
-  }
+const randomize = () => {
+  cardData = getTheData();
+  console.log(cardData);
+  cardData.sort(() => Math.random() - 0.4);
+  return cardData;
+  //console.log(cardData);
+};
 
 const cardGenerator = () => {
   cardData = randomize();
   // Generate the Html
-  cardData.forEach((ele) => {
-  const card = document.createElement('div');
-  const cardFace = document.createElement('img');
-  const cardBack = document.createElement('div');
-  card.classList = 'card'
-  cardFace.classList = 'face'
-  cardBack.classList = 'back'
-  // attach the cards to the section
-  section.appendChild(card);
-  card.appendChild(cardFace);
-  card.appendChild(cardBack);
-  cardFace.src = ele.imgSrc
-   
-  });
-  
-  
+  cardData.forEach((elem) => {
+    const card = document.createElement("div");
+    const cardFace = document.createElement("img");
+    const cardBack = document.createElement("div");
+    card.classList = "card";
+    cardFace.classList = "cardFace";
+    cardBack.classList = "cardBack";
 
+    // Attach the info to the cards
+
+    cardFace.src = elem.imgSrc;
+    card.setAttribute("name", elem.name);
+    card.setAttribute('isClicked', false)
+
+    // Attach the cards to the section
+    section.appendChild(card);
+    card.appendChild(cardFace);
+    card.appendChild(cardBack);
+
+    const flipCard = () => {
+      card.classList.toggle("toggleCard");
+      card.setAttribute('isClicked', true)
+      console.log(card);
+      
+    };
+    card.addEventListener("click", flipCard);
+    const checkCard = () => {
+      const card = document.querySelectorAll(".card");
+       card.classList.add('flipped')
+       const flippedCard = document.querySelectorAll('.flipped')
+    
+      if (flippedCard[0].getAttribute("name") === flippedCard[1].getAttribute("name")) {
+        console.log("matched");
+      } else {
+        console.log("No match");
+      }
+    };
+    console.log(card); 
+  });
 
 };
+
 
 cardGenerator();
