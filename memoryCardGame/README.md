4bc8d27924d332f2c6e3c6f3e53f1920ba064cd3
Badr Boussoufa <110860644+BBoussoufa@users.noreply.github.com>
2022-09-22 23:27:25 -0400

    Add files via upload

--- memoryCardGame/README.md
+++ memoryCardGame/README.md
@@ -2,13 +2,11 @@
 
 ## Description
 
-    The game board consists of sixteen cards arranged randomly in a grid. 
-    The deck is made up of eight different pairs of cards, each with different symbols on one side. 
-    Each turn:
+    The game board consists of sixteen cards arranged randomly in a grid. The deck is made up of eight different pairs of cards, each with different symbols on one side. Each turn:
+
     A player flips one card over to reveal its underlying symbol
     The player then turns over a second card, trying to find the corresponding card with the same symbol
-    If the cards match, both cards stay flipped over. If the cards do not match,
-    both cards are returned to their initial hidden state
+    If the cards match, both cards stay flipped over. If the cards do not match, both cards are returned to their initial hidden state
     The game ends once all cards have been correctly matched.
 
 ## Tools I used
--- memoryCardGame/script.js
+++ memoryCardGame/script.js
@@ -1,12 +1,10 @@
 const section = document.querySelector("section");
 const playerLivesCount = document.querySelector("span");
-const playerLives = 7;
 
-//link text
+let playerLives = 4;
 playerLivesCount.textContent = playerLives;
 
 // get the object
-
 const getTheData = () => [
   { imgSrc: "./Images/imgNature1.jpg", name: "nature1" },
   { imgSrc: "./Images/imgNature2.jpg", name: "nature2" },
@@ -32,10 +30,53 @@ const randomize = () => {
   return cardData;
   //console.log(cardData);
 };
+flipCardArray = [];
+
+const flipCard = (e) => {
+  const card = e.target;
+  card.classList.toggle("toggleCard");
+
+  card.classList.add("flipped");
+  flipCardArray.push(card);
+
+  console.dir(card);
+  console.log(flipCardArray);
+
+  if (flipCardArray.length == 2) {
+    if (
+      flipCardArray[0].getAttribute("name") ===
+      flipCardArray[1].getAttribute("name")
+    ) {
+      console.log("match");
+      console.dir(flipCardArray[0]);
+      for (let i = 0; i <= flipCardArray.length; i++) {
+        flipCardArray.pop();
+      }
+    }
+    if (flipCardArray.length == 2) {
+      console.log("No match");
+
+      for (let i = 0; i < flipCardArray.length; i++) {
+        console.log(i);
+        console.log(flipCardArray.length);
+        let curr = flipCardArray[i];
+        setTimeout(function () {
+          curr.classList.toggle("toggleCard");
+        }, 500);
+      }
+      for (let i = 0; i < 2; i++) {
+        flipCardArray.pop();
+      }
+      console.log("array", flipCardArray);
+      console.log(flipCardArray.length);
+    }
+  }
+};
 
 const cardGenerator = () => {
   cardData = randomize();
   // Generate the Html
+
   cardData.forEach((elem) => {
     const card = document.createElement("div");
     const cardFace = document.createElement("img");
@@ -45,38 +86,18 @@ const cardGenerator = () => {
     cardBack.classList = "cardBack";
 
     // Attach the info to the cards
-
     cardFace.src = elem.imgSrc;
-    card.setAttribute("name", elem.name);
-    card.setAttribute('isClicked', false)
+    cardBack.setAttribute("name", elem.name);
 
     // Attach the cards to the section
-    section.appendChild(card);
+
     card.appendChild(cardFace);
     card.appendChild(cardBack);
+    section.appendChild(card);
+    console.log(card);
 
-    const flipCard = () => {
-      card.classList.toggle("toggleCard");
-      card.setAttribute('isClicked', true)
-      console.log(card);
-      
-    };
     card.addEventListener("click", flipCard);
-    const checkCard = () => {
-      const card = document.querySelectorAll(".card");
-       card.classList.add('flipped')
-       const flippedCard = document.querySelectorAll('.flipped')
-    
-      if (flippedCard[0].getAttribute("name") === flippedCard[1].getAttribute("name")) {
-        console.log("matched");
-      } else {
-        console.log("No match");
-      }
-    };
-    console.log(card); 
   });
-
 };
-
 
 cardGenerator();
